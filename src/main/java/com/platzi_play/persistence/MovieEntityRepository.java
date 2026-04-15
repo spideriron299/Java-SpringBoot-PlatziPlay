4c3d4e0abe207c6f606a5392d1cf181481aec2df
DiegoBadillo <diegoBadillo@live.com>
2026-04-15 13:52:37 -0600

    Clase 14 Captura de parametros con PathVariable

--- src/main/java/com/platzi_play/persistence/MovieEntityRepository.java
+++ src/main/java/com/platzi_play/persistence/MovieEntityRepository.java
@@ -3,6 +3,7 @@ package com.platzi_play.persistence;
 import com.platzi_play.domain.dto.MovieDto;
 import com.platzi_play.domain.repository.MovieRepository;
 import com.platzi_play.persistence.crud.CrudMovieEntity;
+import com.platzi_play.persistence.entity.MovieEntity;
 import com.platzi_play.persistence.mapper.MovieMapper;
 import org.springframework.stereotype.Repository;
 
@@ -21,5 +22,11 @@ public class MovieEntityRepository implements MovieRepository {
     @Override
     public List<MovieDto> getAll() {
         return this.movieMapper.toDto(this.crudMovieEntity.findAll());
+    }
+
+    @Override
+    public MovieDto getById(long id) {
+        MovieEntity movieEntity = this.crudMovieEntity.findById(id).orElse(null);
+        return this.movieMapper.toDto(movieEntity);
     }
 }
--- src/main/java/com/platzi_play/web/controller/MovieController.java
+++ src/main/java/com/platzi_play/web/controller/MovieController.java
@@ -3,11 +3,14 @@ package com.platzi_play.web.controller;
 import com.platzi_play.domain.dto.MovieDto;
 import com.platzi_play.domain.service.MovieService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/movies")
 public class MovieController {
     private final MovieService movieService;
 
@@ -15,9 +18,12 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @GetMapping("/movies")
+    @GetMapping
     public List<MovieDto> getAll() {
-
         return this.movieService.getAll();
+    }
+    @GetMapping("/{id}")
+    public MovieDto getById(@PathVariable long id){
+        return this.movieService.getById(id);
     }
 }
