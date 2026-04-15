208ac06208849ab69523b90f61521dd18abc979f
DiegoBadillo <diegoBadillo@live.com>
2026-04-15 10:06:54 -0600

    Clase 8 Repositorios CRUD

---
+++ src/main/java/com/platzi_play/persistence/crud/CrudMovieEntity.java
@@ -0,0 +1,7 @@
+package com.platzi_play.persistence.crud;
+
+import com.platzi_play.persistence.entity.MovieEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CrudMovieEntity extends CrudRepository<MovieEntity, Long> {
+}
