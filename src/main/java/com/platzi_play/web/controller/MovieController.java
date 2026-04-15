2979dc2f8b48ec5672e061c42c0c4ff6acb43889
DiegoBadillo <diegoBadillo@live.com>
2026-04-15 14:01:50 -0600

    Clase 15 integracion de Response Entity

--- src/main/java/com/platzi_play/web/controller/MovieController.java
+++ src/main/java/com/platzi_play/web/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.platzi_play.web.controller;
 
 import com.platzi_play.domain.dto.MovieDto;
 import com.platzi_play.domain.service.MovieService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +20,17 @@ public class MovieController {
     }
 
     @GetMapping
-    public List<MovieDto> getAll() {
-        return this.movieService.getAll();
+    public ResponseEntity<List<MovieDto>> getAll() {
+        return ResponseEntity.ok(this.movieService.getAll());
     }
     @GetMapping("/{id}")
-    public MovieDto getById(@PathVariable long id){
-        return this.movieService.getById(id);
+    public ResponseEntity <MovieDto> getById(@PathVariable long id){
+        MovieDto movieDto = this.movieService.getById(id);
+
+        if (movieDto == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(movieDto);
     }
 }
