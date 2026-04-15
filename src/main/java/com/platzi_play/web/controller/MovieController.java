077360a11d47d8e96b8254461fcdf615a17a0f30
DiegoBadillo <diegoBadillo@live.com>
2026-04-15 10:15:40 -0600

    Clase 9 Creacion de endpoint Get para consulta de peliculas

---
+++ src/main/java/com/platzi_play/web/controller/MovieController.java
@@ -0,0 +1,22 @@
+package com.platzi_play.web.controller;
+
+import com.platzi_play.persistence.crud.CrudMovieEntity;
+import com.platzi_play.persistence.entity.MovieEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MovieController {
+    private final CrudMovieEntity crudMovieEntity;
+
+    public MovieController(CrudMovieEntity crudMovieEntity) {
+        this.crudMovieEntity = crudMovieEntity;
+    }
+
+    @GetMapping("/movies")
+    public List<MovieEntity> getAll() {
+        return (List<MovieEntity>) this.crudMovieEntity.findAll();
+    }
+}
